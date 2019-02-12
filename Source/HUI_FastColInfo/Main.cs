a35a9b637deca58531d62bd1a8f8382e6ecd1c4a
Densevoid <densevoid@yandex.ru>
2019-02-12 05:09:25 +0300

    Add version 1.0.0.0

---
+++ Source/HUI_FastColInfo/Main.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using Harmony;
+using RimWorld;
+using RimWorld.Planet;
+using Verse;
+using Verse.Sound;
+
+namespace HUI_FastColInfo
+{
+    [StaticConstructorOnStartup]
+    static class HarmonyPatches
+    {
+        static HarmonyPatches()
+        {
+            HarmonyInstance harmony = HarmonyInstance.Create("rimworld.densevoid.hui.fastcolinf");
+            harmony.PatchAll();
+        }
+    }
+
+    public static class FCIUtility
+    {
+        public unsafe static void MakeFCIFloatMenu(Action<InspectTabBase> itemAction, ISelectable selObj)
+        {
+            if (selObj is Thing)
+            {
+                Find.Selector.ClearSelection();
+                Find.Selector.Select(selObj, false, true);
+            }
+            else if (selObj is WorldObject)
+            {
+                Find.WorldSelector.ClearSelection();
+                Find.WorldSelector.Select((WorldObject)selObj, false);
+            }
+
+            Log.Message(selObj.GetType().ToString());
+            IEnumerable<InspectTabBase> tabsList = selObj.GetInspectTabs();
+            List<FloatMenuOption> list = new List<FloatMenuOption>();
+            foreach (InspectTabBase tab in tabsList)
+            {
+                if (tab.IsVisible)
+                {
+                    list.Add(new FloatMenuOption(tab.labelKey.Translate(), () => itemAction(tab)));
+                }
+            }
+            list.Reverse();
+            Find.WindowStack.Add(new FloatMenu(list));
+        }
+
+        public static void ChooseMenuAction(InspectTabBase tab)
+        {
+            tab.OnOpen();
+            if (tab is ITab)
+            {
+                MainTabWindow_Inspect pane = (MainTabWindow_Inspect)Find.MainTabsRoot.OpenTab.TabWindow;
+                pane.OpenTabType = tab.GetType();
+            }
+            if (tab is WITab)
+            {
+                Find.World.UI.inspectPane.OpenTabType = tab.GetType();
+            }
+        }
+    }
+
+}
---
+++ Source/HUI_FastColInfo/Patch/ColonistBarColonistDrawer_HandleClicks.cs
@@ -0,0 +1,46 @@
+﻿using Harmony;
+using RimWorld;
+using RimWorld.Planet;
+using UnityEngine;
+using Verse;
+
+namespace HUI_FastColInfo.Patch
+{
+    [HarmonyPatch(typeof(ColonistBarColonistDrawer), "HandleClicks")]
+    static class Patch_ColonistBarColonistDrawer_HandleClicks
+    {
+        static bool Prefix(Rect rect, Pawn colonist)
+        {
+            if (Event.current.type == EventType.MouseUp && Event.current.button == 1 && Mouse.IsOver(rect))
+            {
+                if (!WorldRendererUtility.WorldRenderedNow)
+                {
+                    Thing colonistOrCorpse;
+                    if (colonist != null && colonist.Dead && colonist.Corpse != null && colonist.Corpse.SpawnedOrAnyParentSpawned)
+                    {
+                        colonistOrCorpse = colonist.Corpse;
+                    }
+                    else
+                    {
+                        colonistOrCorpse = colonist;
+                    }
+
+                    if (colonistOrCorpse.Map == Find.CurrentMap)
+                    {
+                        FCIUtility.MakeFCIFloatMenu(FCIUtility.ChooseMenuAction, colonistOrCorpse);
+                    }
+                }
+
+                else
+                {
+                    if (colonist.IsCaravanMember())
+                    {
+                        Caravan caravan = colonist.GetCaravan();
+                        FCIUtility.MakeFCIFloatMenu(FCIUtility.ChooseMenuAction, caravan);
+                    }
+                }
+}
+            return true;
+        }
+    }
+}
