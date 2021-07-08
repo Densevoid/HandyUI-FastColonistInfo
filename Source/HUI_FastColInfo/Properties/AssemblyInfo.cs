63ec3d530a7bba5043bfafbefc3fa9265be4ad0d
densevoid <densevoid@yandex.ru>
2021-07-08 18:26:17 +0300

    Adopt for 1.3 vesion

--- Source/HUI_FastColInfo/Properties/AssemblyInfo.cs
+++ Source/HUI_FastColInfo/Properties/AssemblyInfo.cs
@@ -1,26 +1,15 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Security;
+using System.Security.Permissions;
 
-// Information about this assembly is defined by the following attributes. 
-// Change them to the values specific to your project.
-
+[assembly: AssemblyVersion("1.0.6987.34870")]
 [assembly: AssemblyTitle("HUI_FastColInfo")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("${AuthorCopyright}")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
-
-[assembly: AssemblyVersion("1.0.*")]
-
-// The following attributes are used to specify the signing key for the assembly, 
-// if desired. See the Mono documentation for more information about signing.
-
-//[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
